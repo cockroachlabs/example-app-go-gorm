4bc9f1bf2ecede8c5dce430c7ffb3369fc34b3a2
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2021-09-15 15:58:06 -0400

    Refactored app

--- main.go
+++ main.go
@@ -1,29 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
+	"os"
 	"time"
 
-	// Import GORM-related packages.
-	"github.com/cockroachdb/cockroach-go/crdb/crdbgorm"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/cockroachdb/cockroach-go/v2/crdb/crdbgorm"
+	"github.com/google/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 // Account is our model, which corresponds to the "accounts" database
 // table.
 type Account struct {
-	ID      int `gorm:"primary_key"`
+	ID      uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
 	Balance int
 }
 
-// Functions of type `txnFunc` are passed as arguments to the
-// `ExecuteTx` wrapper that handles transaction retries
-type txnFunc func(*gorm.DB) error
+// Some global values, for examples
+// The acctIDs global variable tracks the random acctIDs generated.
+var acctIDs []uuid.UUID
 
-func transferFunds(db *gorm.DB, fromID int, toID int, amount int) error {
+// The amount to be transferred between the accounts.
+const transferAmt int = 100
+
+func transferFunds(db *gorm.DB, fromID uuid.UUID, toID uuid.UUID, amount int) error {
 	var fromAccount Account
 	var toAccount Account
 
@@ -31,7 +38,7 @@ func transferFunds(db *gorm.DB, fromID int, toID int, amount int) error {
 	db.First(&toAccount, toID)
 
 	if fromAccount.Balance < amount {
-		return fmt.Errorf("account %d balance %d is lower than transfer amount %d", fromAccount.ID, fromAccount.Balance, amount)
+		return fmt.Errorf("account %s balance %d is lower than transfer amount %d", fromAccount.ID, fromAccount.Balance, amount)
 	}
 
 	fromAccount.Balance -= amount
@@ -46,27 +53,74 @@ func transferFunds(db *gorm.DB, fromID int, toID int, amount int) error {
 	return nil
 }
 
+func insertRows(db *gorm.DB, numRows int) error {
+	// Insert rows into the "accounts" table.
+	log.Printf("Creating %d new rows...", numRows)
+	for i := 0; i < numRows; i++ {
+		newID := uuid.New()
+		newBalance := rand.Intn(10000) + transferAmt
+		if err := db.Create(&Account{ID: newID, Balance: newBalance}).Error; err != nil {
+			return err
+		}
+		acctIDs = append(acctIDs, newID)
+	}
+	return nil
+}
+
+func printBalances(db *gorm.DB) {
+	var accounts []Account
+	db.Find(&accounts)
+	fmt.Printf("Balance at '%s':\n", time.Now())
+	for _, account := range accounts {
+		fmt.Printf("%s %d\n", account.ID, account.Balance)
+	}
+}
+
+func deleteAccounts(db *gorm.DB) error {
+	// Used to tear down the accounts table so we can re-run this
+	// program.
+	err := db.Where("id IN ?", acctIDs).Delete(Account{}).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	// Connect to the "bank" database as the "maxroach" user.
-	const addr = "postgres://{username}:{password}@{hostname}:{port}/bank?sslmode=require"
-	db, err := gorm.Open("postgres", addr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	// Read in connection string
+	scanner := bufio.NewScanner(os.Stdin)
+	log.Println("Enter a connection string: ")
+	scanner.Scan()
+	connstring := os.ExpandEnv(scanner.Text())
 
-	// Set to `true` and GORM will print out all DB queries.
-	db.LogMode(false)
+	// Connect to the "bank" database
+	db, err := gorm.Open(postgres.Open(connstring), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix: "bank.",
+		},
+	})
+	if err != nil {
+		log.Fatal("error configuring the database: ", err)
+	}
 
 	// Automatically create the "accounts" table based on the Account
 	// model.
 	db.AutoMigrate(&Account{})
 
-	// Insert two rows into the "accounts" table.
-	var fromID = 1
-	var toID = 2
-	db.Create(&Account{ID: fromID, Balance: 1000})
-	db.Create(&Account{ID: toID, Balance: 250})
+	// Insert five rows into the "accounts" table.
+	// To handle potential transaction retry errors, we wrap the call
+	// to `insertRows` in `crdbgorm.ExecuteTx`, a helper function for
+	// GORM which implements a retry loop
+	if err := crdbgorm.ExecuteTx(context.Background(), db, nil,
+		func(tx *gorm.DB) error {
+			return insertRows(db, 5)
+		},
+	); err != nil {
+		// For information and reference documentation, see:
+		//   https://www.cockroachlabs.com/docs/stable/error-handling-and-troubleshooting.html
+		fmt.Println(err)
+	}
 
 	// The sequence of steps in this section is:
 	// 1. Print account balances.
@@ -77,8 +131,8 @@ func main() {
 	// Print balances before transfer.
 	printBalances(db)
 
-	// The amount to be transferred between the accounts.
-	var amount = 100
+	fromID := acctIDs[0]
+	toID := acctIDs[0:][rand.Intn(len(acctIDs))]
 
 	// Transfer funds between accounts.  To handle potential
 	// transaction retry errors, we wrap the call to `transferFunds`
@@ -86,7 +140,7 @@ func main() {
 	// implements a retry loop
 	if err := crdbgorm.ExecuteTx(context.Background(), db, nil,
 		func(tx *gorm.DB) error {
-			return transferFunds(tx, fromID, toID, amount)
+			return transferFunds(tx, fromID, toID, transferAmt)
 		},
 	); err != nil {
 		// For information and reference documentation, see:
@@ -100,23 +154,4 @@ func main() {
 	// Delete accounts so we can start fresh when we want to run this
 	// program again.
 	deleteAccounts(db)
-}
-
-func printBalances(db *gorm.DB) {
-	var accounts []Account
-	db.Find(&accounts)
-	fmt.Printf("Balance at '%s':\n", time.Now())
-	for _, account := range accounts {
-		fmt.Printf("%d %d\n", account.ID, account.Balance)
-	}
-}
-
-func deleteAccounts(db *gorm.DB) error {
-	// Used to tear down the accounts table so we can re-run this
-	// program.
-	err := db.Exec("DELETE from accounts where ID > 0").Error
-	if err != nil {
-		return err
-	}
-	return nil
 }
